f9b74c520faf7342530b6a72212a285bae16647f
mujundi <musajundi@gmail.com>
2023-03-26 16:08:57 -0500

    Rename command and clean up 'activate' method

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,12 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { join } from "path";
 import { getContent } from "./content";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-
-	// The commandId parameter must match the command field in package.json
-
-
-
 	let disposable = vscode.commands.registerCommand(
-		"learnxinyminutes.helloWorld",
+		"learnxinyminutes.autoOpenTemplate",
 		() => {
-			const guidesRoot = vscode.Uri.file(join(context.extensionPath, "guides/c++.html.markdown"));
-			console.log(join(context.extensionPath, "guides/c++.html.markdown"));
-			vscode.window.showInformationMessage(
-				"Hello World from learnxinyminutes!"
-			);
-
 			const panel = vscode.window.createWebviewPanel(
 				"markdown.preview",
 				"Learn X in Y Minutes",
@@ -34,17 +17,14 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			);
 
+			const guidesRoot = vscode.Uri.file(join(context.extensionPath, "guides/c++.html.markdown"));
 			const guidesPath = panel.webview.asWebviewUri(guidesRoot);
-
 			const languageId = vscode.window.activeTextEditor?.document.languageId;
-
 			panel.webview.html = getContent(context.extensionPath, languageId);
-			// panel.reveal();
 		}
 	);
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() { }
